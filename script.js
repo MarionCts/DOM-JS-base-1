336e5a1598e4ffc4cdc16b974440e29278946ba6
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-05-23 12:19:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+firstTitle = document.querySelector(`#titre`);
+firstTitle.textContent = "Bonjour, étudiant !";
+
+firstParagraph = document.querySelector(`p`);
+firstParagraph.classList.toggle(`MediumRedText`);
